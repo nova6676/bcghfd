85f6d0a78beacaabb6e9c046ace99985b8738369
nova6676 <134915434+nova6676@users.noreply.github.com>
2024-08-03 11:56:44 +0400

    Add files via upload

---
+++ 02/PP_2_1_2_Spring_HelloWorld/src/main/java/App.java
@@ -0,0 +1,22 @@
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext applicationContext =
+                new AnnotationConfigApplicationContext(AppConfig.class);
+        HelloWorld bean = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+        HelloWorld bean2 = (HelloWorld) applicationContext.getBean("helloworld");
+        System.out.println(bean.getMessage());
+
+
+        Cat catBean = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+
+        Cat catBean3 = (Cat) applicationContext.getBean("cat");
+        System.out.println(catBean.getMessage());
+        System.out.println(bean == bean2);
+        System.out.println(catBean == catBean3);
+    }
+}
